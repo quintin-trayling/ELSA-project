56e75be4f4f52e7ce4316f8b40e407065aeedd45
quintin-trayling <56035638+quintin-trayling@users.noreply.github.com>
2020-01-08 14:18:51 -0500

    Update Edge-Detect.py
    Added comments and help strings.
    Added a break to the loop to stop iterating once the end of the icicle has been found.

--- Edge-Detect.py
+++ Edge-Detect.py
@@ -1,51 +1,74 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[23]:
-
-
-# Icicle Edge Detection Script
-# Using OpenCV, find left and right edges of the icicle.
-# Performed using OpenCV thresholding then analyzing difference in brightness values of threshold.
-
+'''
+ELSA Icicle Edge Detection Script designed to find the left and right edges of the icicle using OpenCV.
+Performed using OpenCV thresholding then analyzing difference in brightness values of threshold.
+There is only one function in this script, EdgeDetect. Use help(EdgeDetect) for more details.
+'''
+#Import required libraries
 import cv2 as cv
 import sys
 import numpy as np
 
 
 def EdgeDetect(imageloc,threshold=110,kernalsize=3,lbound=100,ubound=200):
+'''
+ELSA Edge Detection Function. Takes the following arguments:
 
+imageloc as string: relative path to the icicle image for processing
+threshold as int: Brightness threshold for OpenCV Thresholding
+kernalsize as int: Kernal Size for OpenCV Blurring
+lbound as int: Lower bound for edge detection using OpenCV Canny
+ubound as int: Upper bound for edge detection using OpenCV Canny
+
+And returns the following:
+
+leftedge as list: Pixel locations for left icicle edge, from base to tip
+rightedge as list: Pixel locations for right icicle edge, from base to tip
+'''
+    #Contract kernal size variable for ease of use
     k=kernalsize
     
+    #Try reading the image file, returning an error if read fails
     try:
         img = cv.imread(filename = imageloc,flags = cv.IMREAD_GRAYSCALE)
     except:
         return str("No compatible image found at ",imageloc)
     
+    #Gaussian Blur the icicle image
     blurred = cv.GaussianBlur(src = img, ksize = (k, k), sigmaX = 0.1)
     
+    #Brightness threshold the blurred image to calculate outside edges.
     (t, mask) = cv.threshold(src = blurred, thresh = threshold, maxval = 255,type = cv.THRESH_BINARY)
-
     edges = cv.Canny(mask,lbound,ubound)
     
+    #Initialize arrays for optimal memory usage.
     leftedge = list(np.zeros(len(edges)))
     rightedge = list(np.zeros(len(edges)))
     
+    #Create ints for missed pixels (pixels where no edges can be found) and the ending pixel of the icicle.
     missedpix = 0
     endpix = 0
     
+    #Iterate over the height of the image
     for i in range(0,len(edges)):
+        
+        #Check if 2 or more pixels have been missed
         if missedpix < 2:
+            #If not, try to find the next edge, and save the pixels to the arrays
             try:
                 edgedetection = np.nonzero(edges[i])[0]
                 leftedge[i] = edgedetection[0]
                 rightedge[i]= edgedetection[-1]
+            #If fails, consider this a "missed" pixel
             except:
                 missedpix += 1
                 endpix = i
+        #If 2 or more pixels have been missed, then stop iterating, since this is likely the end of the icicle.
+        else:
+            break
             
+    #Remove array elements beyond the end of the icicle so the image can be easily reversed
     del leftedge[endpix-1:]
     del rightedge[endpix-1:]
         
+    #Return the calculated edges
     return leftedge,rightedge
-
