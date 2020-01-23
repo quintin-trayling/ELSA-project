a898439b225ad6f7dca044984a4d8f21e06847d5
Sam Connolly <59612438+Sam-EN-Connolly@users.noreply.github.com>
2020-01-23 11:27:01 -0500

    fixed typo

---
+++ Time_analysis.py
@@ -0,0 +1,38 @@
+'''
+ELSA Icicle functions for fit parameter comparison and plotting. These function will compare 
+the amplitude (A_r), frquency (w), frequency offset (phi), and aplitude offset (C_r) 
+over the growth of one icicle. 
+'''
+
+#set of functions that plot the various parameters as a function of time and length
+def AmpPlot (amplitues) : 
+    '''
+
+    '''
+    return
+
+def FrequencyPlot (frequncies) : 
+    '''
+
+    '''
+    return
+
+def OffsetPlot (offsets) : 
+    '''
+
+    '''
+    return
+
+def ConstantPlot (constants) : 
+    '''
+
+    '''
+    return 
+
+
+
+def TimeAnalysis (params, errors, times, lengths) : 
+    '''
+
+    '''
+    return 
