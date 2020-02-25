26e2b548ee7881ed7f9c93e80591d1dad98a041e
quintin-trayling <56035638+quintin-trayling@users.noreply.github.com>
2020-02-25 10:11:53 -0500

    Create Monitor-Task.py

---
+++ Monitor-Task.py
@@ -0,0 +1,21 @@
+import Monitoring.Health-Monitor as Health-Monitor
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+
+sched = BackgroundScheduler()
+
+month = datetime.now.month
+day = datetime.now.day
+
+filename = str(month+"-"+day+".log")
+
+datapath = str("/Hum_Temp/"+filename)
+
+@sched.scheduled_job('interval', seconds=5)
+def timed_job():
+    Health-Monitor.ApparatusMonitor(sensordata=datapath,plotpath="/Plots/",lasttime="lastrun.txt")
+	
+sched.start()
+input("Press Enter to shutdown Monitoring ")
+sched.shutdown()
+print("Monitoring code shutdown.")
